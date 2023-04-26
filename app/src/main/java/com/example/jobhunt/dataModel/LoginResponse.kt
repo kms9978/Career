83aa57239e4b8fb9aa20584f4f375f9b30deb55a
rkdms <rkdms4971@naver.com>
2023-04-26 19:42:50 +0900

    jobhunt

--- app/src/main/java/com/example/jobhunt/dataModel/LoginResponse.kt
+++ app/src/main/java/com/example/jobhunt/dataModel/LoginResponse.kt
@@ -1,6 +1,10 @@
 package com.example.jobhunt.dataModel
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginResponse(
+    @SerializedName("access_token")
     val token: String,
+    @SerializedName("refresh_token")
     val refreshToken: String
 )
