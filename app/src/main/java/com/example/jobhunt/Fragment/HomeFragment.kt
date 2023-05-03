68106064d07b69aca5b220ad7784ccfaac653b57
rkdms <rkdms4971@naver.com>
2023-05-04 01:46:08 +0900

    jobhunt

--- app/src/main/java/com/example/jobhunt/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/jobhunt/Fragment/HomeFragment.kt
@@ -1,158 +1,118 @@
 package com.example.jobhunt.Fragment
 
 import RecentRecruitAdapter
-import android.annotation.SuppressLint
+import android.R
 import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.PagerSnapHelper
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jobhunt.Adapter.NewComeRecruitAdapter
-import com.example.jobhunt.R
+import com.example.jobhunt.Service.RecruitService
 import com.example.jobhunt.dataModel.RecentRecruit
-import org.json.JSONObject
-import java.io.IOException
-import java.io.InputStream
+import com.example.jobhunt.databinding.FragmentHomeBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class HomeFragment : Fragment() {
-    // Declare properties for the RecyclerView and list of recent recruit items
-    private lateinit var recentRecruitList: MutableList<RecentRecruit>
-    private lateinit var newcomRecruitList: MutableList<RecentRecruit>
-    private lateinit var searchEditText: EditText
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var recentRecruitAdapter: RecentRecruitAdapter
+    private lateinit var newComeRecruitAdapter: NewComeRecruitAdapter
+    private lateinit var retrofit: Retrofit
+    private lateinit var recruitService: RecruitService
+    private var recentRecruits = mutableListOf<RecentRecruit>()
+    private var newComeRecruits = mutableListOf<RecentRecruit>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        setupRetrofit()
+        setupRecyclerView()
+        setupSearchView()
+        fetchData()
 
+        return binding.root
+    }
 
+    private fun setupRetrofit() {
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://raw.githubusercontent.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        recruitService = retrofit.create(RecruitService::class.java)
+    }
 
-        // fragment_home.xml 레이아웃을 inflate하여 view 객체를 생성한다
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+    private fun setupRecyclerView() {
+        recentRecruitAdapter = RecentRecruitAdapter(mutableListOf())
+        binding.rvRecentRecruit.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = recentRecruitAdapter
+        }
 
+        newComeRecruitAdapter = NewComeRecruitAdapter(mutableListOf())
+        binding.rvNewComeRecruit.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = newComeRecruitAdapter
+        }
+    }
 
+        private fun setupSearchView() {
+            binding.searchCompany.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
 
-// 리사이클러뷰 초기화
-        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_recentRecruit)
-        val recyclerView2 = view.findViewById<RecyclerView>(R.id.rv_newComeRecruit)
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    recentRecruitAdapter.filter.filter(newText)
+                    return false
+                }
+            })
+        }
 
-        // 객체 단위로 넘기기
-        val snapHelper = PagerSnapHelper()
-        snapHelper.attachToRecyclerView(recyclerView)
-
-        recyclerView.layoutManager = GridLayoutManager(context, 3, GridLayoutManager.HORIZONTAL, false)
-        recyclerView2.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
-        searchEditText = view.findViewById(R.id.search_company)
-
-        // 최근 채용 정보 데이터를 읽어온다
-        recentRecruitList = readRecentRecruitData()
-
-        //포지션이 신입만 해당되는 데이터 읽기
-        newcomRecruitList = readnewcomRecruitData()
-
-        // 어댑터를 리사이클러뷰에 설정한다
-        recyclerView.adapter = RecentRecruitAdapter(recentRecruitList)
-        recyclerView2.adapter = NewComeRecruitAdapter(newcomRecruitList)
-
-
-        // 검색어를 입력할 때마다 RecyclerView 갱신
-        searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // 검색어를 이용하여 리스트에 검색 결과를 담는다
-                val filteredList = mutableListOf<RecentRecruit>()
-                for (recentRecruit in recentRecruitList) {
-                    if (recentRecruit.content.contains(s.toString())) {
-                        filteredList.add(recentRecruit)
+        private fun fetchData() {
+            recruitService.getRecruits().enqueue(object : Callback<Map<String, RecentRecruit>> {
+                override fun onResponse(
+                    call: Call<Map<String, RecentRecruit>>,
+                    response: Response<Map<String, RecentRecruit>>
+                ) {
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            recentRecruits.clear()
+                            newComeRecruits.clear()
+                            it.values.forEach { recruit ->
+                                when (recruit.type) {
+                                    "recent" -> {
+                                        recentRecruits.addAll(recruit.data)
+                                        recentRecruitAdapter.submitList(recentRecruits)
+                                    }
+                                    "newcome" -> {
+                                        if (recruit.data.any { it.position == "신입" }) {
+                                            newComeRecruits.addAll(recruit.data.filter { it.position == "신입" })
+                                            newComeRecruitAdapter.submitList(newComeRecruits)
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
-
-                // 어댑터를 갱신한다
-                recyclerView.adapter = RecentRecruitAdapter(filteredList)
-            }
-        })
-
-        return view
-    }
-    private fun readnewcomRecruitData(): MutableList<RecentRecruit> {
-        // 최근 채용 정보 데이터 리스트 생성
-        val newcomRecruitList = mutableListOf<RecentRecruit>()
-
-        try {
-            // 앱 리소스에 있는 raw/news.json 파일을 읽어들인다
-            val inputStream: InputStream = resources.openRawResource(R.raw.news)
-            val jsonString = inputStream.bufferedReader().use { it.readText() }
-            val jsonObject = JSONObject(jsonString)
-
-            // JSON 객체를 반복하며 최근 채용 정보 데이터를 파싱하여 리스트에 추가한다
-            for (key in jsonObject.keys()) {
-                val innerJsonObject = jsonObject.getJSONObject(key)
-                val companyName = key
-                val content = innerJsonObject.getString("content")
-                val imageUrl = innerJsonObject.getString("img")
-                val link = innerJsonObject.getString("link")
-                val position = innerJsonObject.getString("position")
-                val plan = innerJsonObject.getString("plan")
-
-                // "신입"만 해당되는 데이터를 추가. 데이터에 전부 신입이므로 임시로 인턴 삽입
-                if (position.contains("인턴")) {
-                    newcomRecruitList.add(RecentRecruit(companyName, content,position,plan,link,imageUrl))
+                override fun onFailure(call: Call<Map<String, RecentRecruit>>, t: Throwable) {
+                    Log.e(TAG, "Failed to fetch data: ${t.message}")
+                    Toast.makeText(activity, "Failed to fetch data: ${t.message}", Toast.LENGTH_SHORT).show()
                 }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
+            })
         }
-
-        return newcomRecruitList
-
     }
-
-    // 이 부분을 내부 말고 Retrofit2를 이용해서 가져오기.
-    // 최근 채용 정보 데이터를 읽어오는 함수
-    private fun readRecentRecruitData(): MutableList<RecentRecruit> {
-        // 최근 채용 정보 데이터 리스트 생성
-        val recentRecruitList = mutableListOf<RecentRecruit>()
-
-        try {
-            // 앱 리소스에 있는 raw/news.json 파일을 읽어들인다
-            val inputStream: InputStream = resources.openRawResource(R.raw.news)
-            val jsonString = inputStream.bufferedReader().use { it.readText() }
-            val jsonObject = JSONObject(jsonString)
-
-            // JSON 객체를 반복하며 최근 채용 정보 데이터를 파싱하여 리스트에 추가한다
-            for (key in jsonObject.keys()) {
-                val innerJsonObject = jsonObject.getJSONObject(key)
-                val companyName = key
-                val content = innerJsonObject.getString("content")
-                val imageUrl = innerJsonObject.getString("img")
-                val link = innerJsonObject.getString("link")
-                val position = innerJsonObject.getString("position")
-                val plan = innerJsonObject.getString("plan")
-
-                recentRecruitList.add(RecentRecruit(companyName, content,position,plan,link,imageUrl))
-            }
-        } catch (e: IOException) {
-
-            e.printStackTrace()
-
-        }
-
-        return recentRecruitList
-    }
-
-
-
-
-}
