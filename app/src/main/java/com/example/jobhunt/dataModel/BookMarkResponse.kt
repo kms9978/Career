36c9865a723855bbb9d784c60d5cb69aa48c532a
rkdms <rkdms4971@naver.com>
2023-05-06 17:51:25 +0900

    jobhunt

--- app/src/main/java/com/example/jobhunt/dataModel/BookMarkResponse.kt
+++ app/src/main/java/com/example/jobhunt/dataModel/BookMarkResponse.kt
@@ -4,5 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class BookMarkResponse(
     val success: Boolean,
-    val message: String
+    val message: String,
+    val user_bookmark_id: Long // 새로 추가된 북마크의 ID를 포함시킵니다.
 )
